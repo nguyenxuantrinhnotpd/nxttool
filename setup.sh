b02110efd7f0aa78cf53e28ff3d20967bc5aa8bd
nguyenxuantrinhnotpd <142568632+nguyenxuantrinhnotpd@users.noreply.github.com>
2023-08-21 10:08:42 +0700

    Update and rename .github/workflows/index.hml to setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+termux-setup-storage
+apt update
+apt upgrade
+printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
+pkg install python
+pip install --upgrade pip
+pip install requests
+curl -s https://raw.githubusercontent.com/lyquangtoaidz/runtool/main/main -o $home/usr/bin/nxtrinh
+chmod 777 $home/usr/bin/nxtrinh
+
+printf '\n\033[1;32m Gõ \033[1;33mnxtrinh \033[1;32mĐể Vào Tool <3\n\n'
