f013a08f496961034e57cd0c3e4d3c557d9746ec
nguyenxuantrinhnotpd <142568632+nguyenxuantrinhnotpd@users.noreply.github.com>
2023-08-31 11:17:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 // mày vô đây làm con cặc gì?
-home='/data/data/com.termux/files/'
-printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+printf '\n\033[1;32m Hiện (Y/n) Thì Gõ y Rồi Nhấn Enter\nNếu Hiện (Default=N) Thì Nhấn Enter\n\n'
+home='/data/data/com.termux/files/
+printf '\n\033[1;32m Hãy Kiên Nhẫn Chờ Đợi\n\n'
+printf '\n\033[1;32m Đang Cài Dữ Liệu Cho Termux\n\n'
 termux-setup-storage
 apt update
 apt upgrade
-printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
+printf '\n\033[1;32m Đang Cài Môi Trường Python\n\n'
 pkg install python
 pip install --upgrade pip
 pip install requests
@@ -12,4 +14,4 @@ curl -s https://raw.githubusercontent.com/nguyenxuantrinhnotpd/nxttool/main/nxtr
 chmod 777 $home/usr/bin/nxtrinh
 pkg install dos2unix
 dos2unix /data/data/com.termux/files/usr/bin/nxtrinh
-printf '\n\033[1;32m9=> Ghi \033[1;33mnxtrinh \033[1;32mĐể Vào Tool <=8\n\n'
+printf '\n\033[1;32m8=> Ghi \033[1;33mnxtrinh \033[1;32mĐể Được Vào Tool <=8\n\n'
